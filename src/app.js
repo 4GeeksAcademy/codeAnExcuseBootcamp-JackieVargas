b0c3d2a219178d211bf264b371a214b98c689144
jackievivianv <145908828+jackievivianv@users.noreply.github.com>
2023-11-08 19:07:29 +0000

    code an excuse bootcamp

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = "";
+
+  let i = Math.floor(Math.random() * who.length);
+  let j = Math.floor(Math.random() * action.length);
+  let k = Math.floor(Math.random() * what.length);
+  let l = Math.floor(Math.random() * when.length);
+
+  excuse = who[i] + " " + action[j] + " " + what[k] + " " + when[l] + ".";
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
